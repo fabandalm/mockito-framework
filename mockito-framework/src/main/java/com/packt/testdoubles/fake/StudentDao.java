40e60442bb569831d83a649c4332bd24fee2e859
Administrator <Administrator@faa-oracle.localdomain>
2018-06-28 10:33:53 -0400

    first upload

---
+++ mockito-framework/src/main/java/com/packt/testdoubles/fake/StudentDao.java
@@ -0,0 +1,11 @@
+package com.packt.testdoubles.fake;
+
+import java.util.List;
+
+import com.packt.testdoubles.dummy.Student;
+
+public interface StudentDao {
+
+	public void batchUpdate(List<Student> students);
+	
+}
